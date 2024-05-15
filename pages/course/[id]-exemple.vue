8a89428935b1df86cdb70fb224741162344313c8
iyeonghun <twosoull@naver.com>
2024-05-15 11:42:00 +0900

    타입스크립트 사용 및 url 파라미터식 pages 이용하기

---
+++ pages/course/[id]-exemple.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <h2>Course</h2>
+    <div>{{ $route.params }}124</div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<style scoped></style>
